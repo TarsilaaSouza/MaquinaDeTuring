14a899228851eaee5b93c49cb81f987b4b4b3930
Hiago Fernandes <hiago.sousa@ccc.ufcg.edu.br>
2018-03-13 13:13:04 -0300

    logica da maquina

---
+++ turing-machine/code/maquina/Maquina.py
@@ -0,0 +1,13 @@
+from Arquivo import Arquivo
+from Operacao import Operacao
+from Fita import Fita
+
+ #codificao dos simbolos
+
+ESTADO_INICIAL = '0'
+ACEITACAO = 'halt-accept'
+REJEICAO = 'halt-reject'
+
+class Maquina:
+
+    
