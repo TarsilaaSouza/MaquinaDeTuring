b498add6141c0bdb73e336fa555499e9938686e9
Hiago Fernandes <hiago.sousa@ccc.ufcg.edu.br>
2018-03-13 11:49:10 -0300

    fim dos arquivos

--- turing-machine/code/maquina/Arquivo.py
+++ turing-machine/code/maquina/Arquivo.py
@@ -12,5 +12,13 @@ def lerArquivo(caminho):
     estados_gerados = {}
 
     for linha in arquivo:
+
         if not linha.startswith(SIMBOLO_DE_COMENTARIO) and not linha.startswith("\n") and len(linha) > 2:
             linha = linha.split(" ")
+            linha[-1] = linha[-1].strip("\n")
+            estado_gerado = Estado(linha[0], linha[1], linha[2], linha[3], linha[4])
+            estados_gerados[(estado_gerado.nome, estado_gerado.simbolo)] = estado_gerado
+
+    arquivo.close()
+
+    return estados_gerados
