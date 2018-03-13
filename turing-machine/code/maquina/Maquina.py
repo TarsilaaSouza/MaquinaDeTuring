728f1c4edb171b5aed9afc8a3cb4dae1dd28f519
Hiago Fernandes <hiago.sousa@ccc.ufcg.edu.br>
2018-03-13 13:55:37 -0300

    logica da maquina

--- turing-machine/code/maquina/Maquina.py
+++ turing-machine/code/maquina/Maquina.py
@@ -24,8 +24,27 @@ class Maquina:
     def inserirFita(self, entrada_fita):
         self.Fita.inserirFita(entrada_fita)
 
-    def print_info_atual(self, Fita, estado_atual_nome, qtdPassos):
+    def print_informacao_atual(self, Fita, estado_atual_nome, qtdPassos):
         print("Estado atual: %s - Posicao: %d - Passos: %d" % (estado_atual_nome, Fita.simbolo_topo, qtdPassos))
+
+    def print_informacao_fita(self, Fita):
+        print("Fita -> \n" + (Fita.simbolo_topo)
+        print(Fita)
+
+    def verifica_parada(self, nome_ProxEstado, Fita, qtdPassos):
+
+        if nome_ProxEstado == ACEITACAO or nome_ProxEstado == REJEICAO:
+            self.print_informacao_atual(self.Fita, nome_ProxEstado, qtdPassos)
+            self.print_informacao_fita(self.Fita)
+            print("FIM!")
+            return True
+        elif nome_ProxEstado == "halt":
+            self.print_informacao_atual(self.Fita, nome_ProxEstado, qtdPassos)
+            self.print_informacao_fita(self.Fita)
+            print("FIM!")
+            return True
+        else:
+            return False
 
     def startMaquina(self):
 
@@ -35,4 +54,17 @@ class Maquina:
         while True:
 
             if estado_atual is not None:
-                self.print_info_atual(self.Fita, estado_atual.nome, qtdPassos)
+                self.print_informacao_atual(self.Fita, estado_atual.nome, qtdPassos)
+                self.print_informacao_fita(self.Fita)
+                qtdPassos += 1
+                self.Fita.escrever(estado_atual.novo_simbolo)
+                self.Fita.mover(estado_atual.direcao)
+
+                if verifica_parada(estado_atual.proximo_estado, self.Fita, qtdPassos): break
+
+                estado_auxiliar = estado_atual
+                estado_atual = self.Operacao.proxEstado(estado_auxiliar.proximo_estado, self.Fita.simbolo_topo)
+
+            else:
+                print ("---> FINALIDADO! <---")
+                break
