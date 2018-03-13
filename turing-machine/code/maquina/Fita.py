383ba274b7a941d6ecbce224bbb9d64be5720f48
Hiago Fernandes <hiago.sousa@ccc.ufcg.edu.br>
2018-03-13 11:37:10 -0300

    fim da fita

--- turing-machine/code/maquina/Fita.py
+++ turing-machine/code/maquina/Fita.py
@@ -8,21 +8,55 @@ DIREITA = 'r'
 
 class Fita(object):
 
-    def __init__(self, posicao_inicio=1, elementos=[]):
-        self.posicao_inicio=posicao_inicio
+    def __init__(self, apontador=1, elementos=[]):
+        self.apontador=apontador
         self.elementos=elementos
 
     def get_Fita(self):
         return self.elementos
 
     def simbolo_topo(self):
-        return self.elementos[self.posicao_inicio]
+        return self.elementos[self.apontador]
 
     def simbolo_inicial(self):
-        return self.posicao_inicio
+        return self.apontador
 
     def inserir(self, entrada):
         self.elementos=[]
         self.elementos.append(SEM_ESCRITA)
         self.elementos += list(input.replace(" ", SEM_ESCRITA))
         self.elementos.append(SEM_ESCRITA)
+
+    def mover(self, direcao):
+        if direcao == DIREITA:
+            if self.apontador == len(self.elementos) - 1:
+                self.elementos.append(SEM_ESCRITA)
+                self.apontador += 1
+            else:
+                self.apontador += 1
+
+        if direcao = ESQUERDA:
+            if self.apontador == 0:
+                self.elementos = [SEM_ESCRITA] + self.elementos
+                self.apontador = 0
+            else:
+                self.apontador -= 1
+
+    def escrever(self, simbolo):
+        if simbolo != USADO:
+            self.elementos[self.apontador] = simbolo
+
+    def ponteiro(self):
+        indicador = ""
+        for e in range(len(self.elementos)):
+            if e == apontador:
+                indicador += 1
+            else:
+                indicador += " "
+        return indicador
+
+    def info(self):
+        return str(self.elementos)
+
+    def str(self):
+        return " ".join(self.elementos).replace("_", " ")
