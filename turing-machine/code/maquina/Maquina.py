24b27a1654621483f54cc92d869cbc70dc81fe35
Hiago Fernandes <hiago.sousa@ccc.ufcg.edu.br>
2018-03-13 13:13:33 -0300

    logica da maquina

--- turing-machine/code/maquina/Maquina.py
+++ turing-machine/code/maquina/Maquina.py
@@ -10,4 +10,25 @@ REJEICAO = 'halt-reject'
 
 class Maquina:
 
-    
+    def __init__(self, Fita=Fita(), Operacao=Operacao()):
+        self.Fita = Fita
+        self.Operacao = Operacao
+
+    def existPrograma(self):
+        return self.Operacao.existEstado()
+
+    def carregamento(self, caminhoDoArquivo):
+        estados = Arquivo(caminhoDoArquivo)
+        self.Operacao.setEstados(estados)
+
+    def inserirFita(self, entrada_fita):
+        self.Fita.inserirFita(entrada_fita)
+
+    def startMaquina(self):
+
+        qtdPassos = 0
+        estado_atual = self.Operacao.proxEstado(ESTADO_INICIAL, self.Fita.simbolo_topo)
+
+        while True:
+
+                    
